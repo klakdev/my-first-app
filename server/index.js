121c8ce672ca522910313d3c5895d0bb803220f1
Yaki <yaki.klein.dev@gmail.com>
2021-01-02 20:58:39 +0200

    some additional comments ans set post from env

--- server/index.js
+++ server/index.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser'); 
 const cookieParser = require('cookie-parser'); 
+
 const user = require("./user");
 const post = require("./post");
+
+//create a new express application
 const app = express()
-const port = 3000
+//get post for server to listen on
+const port = process.env.PORT || 3001
 
-
+//middleware for parsing the http body and cookie header
 app.use(bodyParser.json());
 app.use(cookieParser());
+
+//routes for handling users and posts
 app.use(user.ROUTE_PATH, user.route);
 app.use(post.ROUTE_PATH, post.route);
 
+//make application listen on requested port
 app.listen(port, () => {
   console.log(`listening at http://localhost:${port}`)
 })
